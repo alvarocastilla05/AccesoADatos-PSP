bffcd3d2ab0d7f6922249cb77cd3918c1e13a9a7
alvarocastilla05 <castilla.caalv24@triana.salesianos.edu>
2024-10-02 13:34:41 +0200

    Api bar a medias

--- EjercicioBAres/src/main/java/com/salesianostriana/dam/ejerciciobares/repositories/BarRepository.java
+++
@@ -1,7 +0,0 @@
-package com.salesianostriana.dam.ejerciciobares.repositories;
-
-import com.salesianostriana.dam.ejerciciobares.models.Bar;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BarRepository extends JpaRepository<Bar, Long> {
-}
