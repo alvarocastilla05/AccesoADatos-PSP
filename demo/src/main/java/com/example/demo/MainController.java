8b0ff3210cf3dbf727f04757c6cebbd8cf91dfd6
alvarocastilla05 <castilla.caalv24@triana.salesianos.edu>
2024-12-11 11:35:32 +0100

    Cosas

--- demo/src/main/java/com/example/demo/MainController.java
+++ demo/src/main/java/com/example/demo/MainController.java
@@ -1,6 +1,8 @@
 package com.example.demo;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -14,6 +16,11 @@ public class MainController {
     @GetMapping("/producto/123")
     public Producto getProducto() {
         return new Producto(123L, "Un producto molón");
+    }
+
+    @PostMapping("/producto")
+    public Producto nuevoProducto(@RequestBody Producto producto) {
+        return producto;
     }
 
     record Producto (Long id, String nombre) {
