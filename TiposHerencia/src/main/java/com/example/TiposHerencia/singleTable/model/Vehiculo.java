1ba4738dc80c86ea83af12c523f39388ea5a3f5f
alvarocastilla05 <castilla.caalv24@triana.salesianos.edu>
2025-01-24 13:23:16 +0100

    SuperBuilder

--- TiposHerencia/src/main/java/com/example/TiposHerencia/singleTable/model/Vehiculo.java
+++ TiposHerencia/src/main/java/com/example/TiposHerencia/singleTable/model/Vehiculo.java
@@ -2,6 +2,7 @@ package com.example.TiposHerencia.singleTable.model;
 
 import jakarta.persistence.*;
 import lombok.*;
+import lombok.experimental.SuperBuilder;
 
 import java.time.LocalDate;
 
@@ -11,6 +12,7 @@ import java.time.LocalDate;
 @Entity
 @Getter
 @Setter
+@SuperBuilder
 @Inheritance(strategy = InheritanceType.SINGLE_TABLE)
 public abstract class Vehiculo {
 
