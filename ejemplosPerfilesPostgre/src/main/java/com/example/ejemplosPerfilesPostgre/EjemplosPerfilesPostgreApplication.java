a3228c5973d0208e78ebf9e0db956845d0757bcc
alvarocastilla05 <castilla.caalv24@triana.salesianos.edu>
2025-01-27 14:27:44 +0100

    Comienzo ejemplo perfiles

---
+++ ejemplosPerfilesPostgre/src/main/java/com/example/ejemplosPerfilesPostgre/EjemplosPerfilesPostgreApplication.java
@@ -0,0 +1,13 @@
+package com.example.ejemplosPerfilesPostgre;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EjemplosPerfilesPostgreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EjemplosPerfilesPostgreApplication.class, args);
+	}
+
+}
---
+++ ejemplosPerfilesPostgre/src/test/java/com/example/ejemplosPerfilesPostgre/EjemplosPerfilesPostgreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ejemplosPerfilesPostgre;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EjemplosPerfilesPostgreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
