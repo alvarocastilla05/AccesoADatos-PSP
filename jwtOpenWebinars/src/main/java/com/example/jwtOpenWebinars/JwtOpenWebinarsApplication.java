f04787e1e4905ad57915820a1932e80bb34741f0
alvarocastilla05 <castilla.caalv24@triana.salesianos.edu>
2025-02-11 13:09:14 +0100

    Comienzo curso OPENWEBINARS

---
+++ jwtOpenWebinars/src/main/java/com/example/jwtOpenWebinars/JwtOpenWebinarsApplication.java
@@ -0,0 +1,13 @@
+package com.example.jwtOpenWebinars;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtOpenWebinarsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwtOpenWebinarsApplication.class, args);
+	}
+
+}
---
+++ jwtOpenWebinars/src/test/java/com/example/jwtOpenWebinars/JwtOpenWebinarsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jwtOpenWebinars;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtOpenWebinarsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
